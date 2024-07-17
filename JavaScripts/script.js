a3eadb50226de2b41fe93b4b85c77df7acbe4f39
Olane2010 <153990160+Olane2010@users.noreply.github.com>
2024-07-17 22:19:58 +0500

    Password Update!

--- JavaScripts/script.js
+++ JavaScripts/script.js
@@ -1,1 +1,16 @@
-console.log("Working!")
+alert ("Добро Пожаловать на страницу 7В класса")
+showprompt();
+
+function showprompt()
+{
+    let password = +prompt("Введите пароль для входа","")
+    if(password != 34148)
+    {
+        alert("Пароль не правильный!")
+        showprompt();
+    }
+    else{
+        
+    }
+    
+}
