1965ab27f87ef70c0a3ba6a5bd132ef482c70669
Olane2010 <153990160+Olane2010@users.noreply.github.com>
2024-07-18 12:29:43 +0500

    Add window

--- js/script.js
+++ js/script.js
@@ -19,3 +19,21 @@ function OnClick()
 {
     alert("В разработке!");
 }
+
+const modal = document.querySelector('#modal');
+const btn = document.querySelector('#openModal');
+const close = document.querySelector('.close');
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+close.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
